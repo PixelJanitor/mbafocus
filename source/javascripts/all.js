5c91c0e5a4317f1f2598f8c2dfc62564a040d697
Bryan Cunningham <bryan.cunningham@me.com>
2015-02-23 20:13:26 -0500

    Add job listings table. Begin work on next section of home page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -9,4 +9,16 @@ $(function() {
       },
     }
   });
+
+  $(document).on("click", ".fa-star-o", function() {
+    var fullStar = $(this).next(".fa-star");
+    $(this).addClass("hide").removeClass("show");
+    fullStar.removeClass("hide").addClass("show")
+  });
+
+  $(document).on("click", ".fa-star", function() {
+    var emptyStar = $(this).prev(".fa-star-o");
+    $(this).addClass("hide").removeClass("show");
+    emptyStar.removeClass("hide").addClass("show")
+  });
 });
