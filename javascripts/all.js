5517a9861d036b99d6245b314c823e2992ddad05
Bryan Cunningham <bryan.cunningham@me.com>
2015-03-03 20:45:45 -0500

    Automated commit at 2015-03-04 01:45:45 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -3088,7 +3088,36 @@ $(function() {
     emptyStar.removeClass("hide").addClass("show")
   });
 
-  $('.calendar-container').clndr({
-    template: $('#calendar').html()
+  var calendar = $('.calendar-container').clndr({
+    template: $('#calendar').html(),
+    clickEvents: {
+      // fired whenever a calendar box is clicked.
+      // returns a 'target' object containing the DOM element, any events, and the date as a moment.js object.
+      click: function(target){ },
+      // fired when a user goes forward a month. returns a moment.js object set to the correct month.
+      nextMonth: function(month){
+        calendar.forward();
+      },
+      // fired when a user goes back a month. returns a moment.js object set to the correct month.
+      previousMonth: function(month){
+        calendar.back();
+      },
+      // fired when a user goes back OR forward a month. returns a moment.js object set to the correct month.
+      onMonthChange: function(month){
+        console.log(month);
+      },
+      // fired when a user goes to the current month/year. returns a moment.js object set to the correct month.
+      today: function(month){ }
+    },
+    events: [
+      { date: moment("2015-03-03").format("MM-DD-YY"),
+        name: "Coaching",
+        location: "The diamond"
+      },
+      { date: moment("2015-03-06").format("MM-DD-YY"),
+        name: "Practicing Duck Call",
+        location: "Wherever I want"
+      },
+    ]
   });
 });
